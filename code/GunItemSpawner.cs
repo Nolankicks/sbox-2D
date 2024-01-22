e265e837fd7753f574eeee5e2f55437a11de32f8
Nolankicks <nolandimensions@gmail.com>
2024-01-22 14:08:58 -0500

    Update health boost and power up spawn times, add GunManager component

--- code/GunItemSpawner.cs
+++ code/GunItemSpawner.cs
@@ -1,9 +1,23 @@
+using System;
+using System.Security.Cryptography;
 using Sandbox;
 
 public sealed class GunItemSpawner : Component
 {
-	protected override void OnUpdate()
-	{
+    
+    float GetRandomNumber() => Random.Shared.Float(0, 100);
+    [Property] public GameObject gun {get; set;}
+    protected override void OnFixedUpdate()
+    {
+		GetRandomNumber();
+		var random = GetRandomNumber();
+        
+        Log.Info(random);
+		if (random > 99.9f)
+		{
+			var gunClone = gun.Clone();
+			gunClone.Transform.Position = new Vector3(0, Random.Shared.Float(-150, 150), 0);
+		}
+    }
+    }
 
-	}
-}
