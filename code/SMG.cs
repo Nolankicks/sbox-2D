472758e2ebcbe3474b0cf329236e55d216d578e0
Nolankicks <nolandimensions@gmail.com>
2024-01-24 11:53:00 -0500

    Add SMG and player animation to 2dmain scene

---
+++ code/SMG.cs
@@ -0,0 +1,44 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+public sealed class SMG : Component
+{
+	[Property] public GameObject impactEffect { get; set; }
+	[Property] public CitizenAnimationHelper playerAnimation { get; set; }
+	TimeSince timeSinceShoot;
+	protected override void OnUpdate()
+	{
+		if (Input.Down("attack2"))
+		{
+			Shoot();
+			
+		}
+	}
+
+	public void Shoot()
+	{
+		if (timeSinceShoot < 0.1f) return;
+		timeSinceShoot = 0;
+		var camFoward = playerAnimation.EyeWorldTransform.Position;
+		var tr = Scene.Trace.Ray(camFoward, camFoward + camFoward * 5000).WithTag("test").Run();
+
+		if (!tr.Hit) return;
+
+
+		if (tr.Hit)
+		{
+			Log.Info("Hit");
+			var trgo = tr.GameObject;
+			trgo.Destroy();
+		}
+		else
+		{
+			Log.Info("Miss");
+		}
+		if (impactEffect is not null)
+		{
+			impactEffect.Clone(new Transform(tr.HitPosition + Vector3.Up * 64, Rotation.LookAt(tr.Normal)));
+		}
+
+	}
+}
