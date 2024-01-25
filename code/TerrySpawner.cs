268e27db54fa6ddfccbc1468b2bc9bf5a7948158
Nolankicks <nolandimensions@gmail.com>
2024-01-25 14:44:58 -0500

    Update health boost spawn time and add spawn point retrieval in TerrySpawner.cs

--- code/TerrySpawner.cs
+++ code/TerrySpawner.cs
@@ -19,6 +19,7 @@ public sealed class TerrySpawner : Component
 	}
 	void SpawnTerry()
 	{
+		var spawn = Scene.Components.GetAll<SpawnPoint>();
 		var cc = Scene.Components.GetAll<PlayerController>().FirstOrDefault();
 		var terryGo = terry.Clone();
 		terryGo.Transform.Position = new Vector3(0, Random.Shared.Float(-200, 200), 0);
