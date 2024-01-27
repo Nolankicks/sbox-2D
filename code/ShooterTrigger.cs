074ff435c081adcfe872c00022a102738a26c6d0
Nolankicks <nolandimensions@gmail.com>
2024-01-27 14:01:50 -0500

    Update game assets and code

--- code/ShooterTrigger.cs
+++ code/ShooterTrigger.cs
@@ -8,11 +8,13 @@ public sealed class ShooterTrigger : Component, Component.ITriggerListener //Cha
 {
 
  bool _iTouching; 
-[Property] public GameObject badguy {get; set;}
+
 [Property] public GameObject particleEffect {get; set;}
-[Property] public GameObject zombieRagdoll {get; set;}
+
 [Property] public GameObject humanRagdoll {get; set;}
+[Property] public GameObject zombieRagdoll {get; set;}
 Manager manager => Scene.GetAllComponents<Manager>().FirstOrDefault();
+[Property] SkinnedModelRenderer body {get; set;}
 
  public void istouching(bool _iTouching)
  {
@@ -34,28 +36,28 @@ Manager manager => Scene.GetAllComponents<Manager>().FirstOrDefault();
     {
 		if (!other.GameObject.IsValid)
 		return;
-		var pos = badguy.Transform.Position;	
-		var rot = badguy.Transform.Rotation;	
+		var pos = body.Transform.Position;	
+		var rot = body.Transform.Rotation;	
 		if (other.GameObject.Tags.Has("playerbullet"))
 		{
 
 			
-			badguy.Destroy();
+			GameObject.Parent.Destroy();
 			
 			if (GameObject.Tags.Has("human"))
 			{
 			humanRagdoll.Clone(pos + Vector3.Backward * 75, rot);
 			}
-			else
+			if (GameObject.Tags.Has("zombie"))
 			{
 			zombieRagdoll.Clone(pos + Vector3.Backward * 75, rot);
 			}
-			manager.AddScore();
-			particleEffect.Clone(pos);
-			
+
+		manager.AddScore();
+		particleEffect.Clone(pos);
 		}		
 			
-		
+
 		//Log.Info( "Triggered" );
         //managerref.EndGame();
 		//healthManager.healthNumber = 0;
@@ -63,16 +65,15 @@ Manager manager => Scene.GetAllComponents<Manager>().FirstOrDefault();
         
     }
 
-    void ITriggerListener.OnTriggerExit( Collider other ) 
+	 public void OnTriggerExit( Collider other )
     {
-		
-		
 
-	
-    
 
-    }
+
+	}
 }
+
+
 	
 
 	
